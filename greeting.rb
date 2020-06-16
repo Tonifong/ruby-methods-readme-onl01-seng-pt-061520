ab7a41765511f64e13296d9322117d6eebbe65e1
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-16 21:08:21 +0000

    Done.

---
+++ greeting.rb
@@ -0,0 +1,18 @@
+def greeting
+  puts "Hello World"
+end
+ 
+greeting
+greeting
+greeting
+greeting
+greeting
+
+
+$ ruby greeting.rb
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+$
